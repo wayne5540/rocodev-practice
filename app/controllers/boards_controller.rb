56c37234f22d18c6272bfcae78fff3eb7f2d6440
wayne5540 <w5540@yahoo.com.tw>
2014-05-06 11:34:58 +0800

    board's crud

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   def index
-    @board = Board.all
+    @boards = Board.all
   end
 
   def show
@@ -12,7 +12,14 @@ class BoardsController < ApplicationController
   end
 
   def create
-    
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+      flash[:success] = "Board created!"
+    else
+      render :new
+      flash[:warning] = "Failed to build Board"
+    end
   end
 
   def edit
@@ -20,11 +27,21 @@ class BoardsController < ApplicationController
   end
 
   def update
-    
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(@board)
+      flash[:success] = "Update success!"
+    else
+      render :edit
+      flash[:warning] = "Update failed!"
+    end
   end
 
   def destroy
-    
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
+    flash[:success] = "Delete success!"
   end
 
 private
