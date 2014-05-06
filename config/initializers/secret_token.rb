ff83a6a4f533405d672e2a7121eb409349344634
wayne5540 <w5540@yahoo.com.tw>
2014-05-06 10:42:11 +0800

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RocodevPractice::Application.config.secret_key_base = '0c57237b3b77bd79778ea21cf87ceef333072835c3057d12e1a688853fbc218f3144a32a76defcc05eda88ee63d320317f63aa167ad3d8c9cb23eb37d2e3b540'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RocodevPractice::Application.config.session_store :cookie_store, key: '_rocodev_practice_session'
