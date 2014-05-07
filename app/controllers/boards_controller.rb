1e71d878d99995ef6a026e6e0877dc7d8bf832ec
wayne5540 <w5540@yahoo.com.tw>
2014-05-07 19:03:50 +0800

    namspace

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,5 @@
 class BoardsController < ApplicationController
 
-  before_action :admin_required, :only => [:new, :create, :edit, :update, :destroy]
-
   def index
     @boards = Board.all
   end
@@ -10,46 +8,4 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
-  def new
-    @board = current_user.boards.new
-  end
-
-  def create
-    @board = current_user.boards.new(board_params)
-    if @board.save
-      redirect_to board_path(@board)
-      flash[:success] = "Board created!"
-    else
-      render :new
-      flash[:warning] = "Failed to build Board"
-    end
-  end
-
-  def edit
-    @board = current_user.boards.find(params[:id])
-  end
-
-  def update
-    @board = current_user.boards.find(params[:id])
-    if @board.update(board_params)
-      redirect_to board_path(@board)
-      flash[:success] = "Update success!"
-    else
-      render :edit
-      flash[:warning] = "Update failed!"
-    end
-  end
-
-  def destroy
-    @board = current_user.boards.find(params[:id])
-    @board.destroy
-    redirect_to boards_path
-    flash[:success] = "Delete success!"
-  end
-
-private
-
-  def board_params
-    params.require(:board).permit(:name, :description, :user_id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ RocodevPractice::Application.routes.draw do
   resources :boards do
     resources :topics
   end
+  namespace :admin do 
+    resources :boards
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
