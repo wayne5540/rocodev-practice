6ad83f9ca3a7641e54d8e8c45ffbb087238500a7
wayne5540 <w5540@yahoo.com.tw>
2014-05-10 11:03:12 +0800

    account helper

--- app/helpers/account/topics_helper.rb
+++ app/helpers/account/topics_helper.rb
@@ -1,2 +1,31 @@
 module Account::TopicsHelper
+
+  def render_topic_content(topic)
+    topic.content    
+  end
+
+  def render_topic_id(topic)
+    topic.id
+  end
+
+  def render_topic_title(topic)
+    topic.title
+  end
+
+  def avatar_exists?(topic)
+    topic.avatar.exists?
+  end
+
+  def render_board_topic_path(topic)
+    board_topic_path(topic, topic.board_id)
+  end
+
+  def render_edit_account_topic_path(topic)
+    edit_account_topic_path(topic.board_id, topic)
+  end
+
+  def render_account_topic_path(topic)
+    account_topic_path(topic.board_id, topic)   
+  end
+
 end
