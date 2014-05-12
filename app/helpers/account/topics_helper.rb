14d993f132c3027290e8a45b1a9a624b85ded5ec
wayne5540 <w5540@yahoo.com.tw>
2014-05-12 10:18:07 +0800

    board and topic view refactor

--- app/helpers/account/topics_helper.rb
+++ app/helpers/account/topics_helper.rb
@@ -1,24 +1,2 @@
 module Account::TopicsHelper
-
-  def render_topic_content(topic)
-    topic.content    
-  end
-
-  def render_topic_id(topic)
-    topic.id
-  end
-
-  def render_topic_title(topic)
-    topic.title
-  end
-
-  def avatar_exists?(topic)
-    topic.avatar.exists?
-  end
-
-  def render_board_topic_path(topic)
-    board_topic_path(topic.board_id, topic)
-  end
-
-
 end
