603eadb6dbe8ff54ac7347316915b58d388c3138
wayne5540 <w5540@yahoo.com.tw>
2014-05-13 10:29:46 +0800

    helper method refactor

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -8,8 +8,16 @@ module BoardsHelper
     board.description
   end
 
-  def render_board_id(board)
-    board.id
+  def render_board_name_path(board)
+    link_to(board.name, board_path(board)) 
+  end
+
+  def render_edit_admin_board_path(board)
+    link_to("edit", edit_admin_board_path(board), :class => "btn btn-default") 
+  end
+
+  def render_destroy_admin_board_path(board)
+    link_to("delete", admin_board_path(board), :class => "btn btn-default", :method => :delete, :confirm => "Are you sure?")
   end
 
 end
