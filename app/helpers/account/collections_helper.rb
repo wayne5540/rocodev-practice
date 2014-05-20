4c2d3cc5dd4b3ff7ee36b7517aff9133773ceda4
wayne5540 <w5540@yahoo.com.tw>
2014-05-20 14:17:40 +0800

    collection helper logic

--- app/helpers/account/collections_helper.rb
+++ app/helpers/account/collections_helper.rb
@@ -4,10 +4,10 @@ module Account::CollectionsHelper
       return link_to("登入後即可收藏文章", "#", :class => "btn btn-success", :disabled => true)
     end
 
-    if user && !user.has_collection?(topic)
-      link_to("收藏", account_collections_path(id: topic), :class => "btn btn-success", method: :post, remote: true)
-    else
+    if user.has_collection?(topic)
       link_to("取消收藏", account_collection_path(id: topic), :class => "btn btn-warning", method: :delete, remote: true)
+    else
+      link_to("收藏", account_collections_path(id: topic), :class => "btn btn-success", method: :post, remote: true)
     end
   end
 end
